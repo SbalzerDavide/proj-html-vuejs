f8ca830c0fd742e065a6cba8afad56bf37db1359
Davide <Sbalzerdavide@hotmail.it>
2020-11-30 13:03:34 +0100

    start navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        prova:'ciaono',
+        phoneNumber: '+1 (305) 1234-5678',
+        mail: 'hello@example.com',
+        OpenHours: {
+            openDay: 'Mon',
+            closeDay: 'Sat',
+            openHour: '9.00',
+            closeHour: '18.00',
+        },
+        menu: [
+            {
+                name: 'home',
+                sub: [],
+            },
+            {
+                name: 'about',
+                sub: [],
+            },
+            {
+                name: 'services',
+                sub: [],
+            },
+            {
+                name: 'pricing',
+                sub: [],
+            },
+            {
+                name: 'blog',
+                sub: [],
+            },
+        ]
     },
 });
