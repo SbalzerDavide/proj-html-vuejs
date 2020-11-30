586508708190a7e9625a1ade983db098a44c54a3
Davide <Sbalzerdavide@hotmail.it>
2020-11-30 22:59:35 +0100

    pricing

--- js/main.js
+++ js/main.js
@@ -30,6 +30,93 @@ const app = new Vue ({
                 name: 'blog',
                 sub: [],
             },
+        ], 
+        plans: [
+            {
+                name: 'Standard',
+                cost: 490,
+                icon: 'fas fa-space-shuttle',
+                popular: false,
+                features:[
+                    {
+                        name: 'Standard Accounting',
+                        status: true,
+                    },
+                    {
+                        name: 'Platform Access',
+                        status: true,
+                    },
+                    {
+                        name: 'Business Orentation',
+                        status: false,
+                    },
+                    {
+                        name: 'Dedicated Consultant',
+                        status: false,
+                    },
+                    {
+                        name: 'Personal Assistance',
+                        status: false,
+                    },
+                ]
+            },
+            {
+                name: 'Professional',
+                cost: 890,
+                icon: 'fas fa-briefcase',
+                popular: true,
+                features:[
+                    {
+                        name: 'Standard Accounting',
+                        status: true,
+                    },
+                    {
+                        name: 'Platform Access',
+                        status: true,
+                    },
+                    {
+                        name: 'Business Orentation',
+                        status: true,
+                    },
+                    {
+                        name: 'Dedicated Consultant',
+                        status: false,
+                    },
+                    {
+                        name: 'Personal Assistance',
+                        status: false,
+                    },
+                ]
+            },
+            {
+                name: 'Ultimate',
+                cost: 1390,
+                icon: 'far fa-gem',
+                popular: false,
+                features:[
+                    {
+                        name: 'Standard Accounting',
+                        status: true,
+                    },
+                    {
+                        name: 'Platform Access',
+                        status: true,
+                    },
+                    {
+                        name: 'Business Orentation',
+                        status: true,
+                    },
+                    {
+                        name: 'Dedicated Consultant',
+                        status: true,
+                    },
+                    {
+                        name: 'Personal Assistance',
+                        status: true,
+                    },
+                ]
+            },
+
         ]
     },
 });
